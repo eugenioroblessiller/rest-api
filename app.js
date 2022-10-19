a507ce585edc65193639ea3f6b96718895deebef
eugenio <eugenio@baliotech.com>
2022-10-18 23:19:24 -0500

    change the routes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const indexRouter = require('./routes/web/index');
 // const usersRouter = require('./routes/web/users');
 const usersRouter = require('./routes/api/users')
 
-  
+
 const app = express();
 
 // view engine setup
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 app.use('/', indexRouter);
-app.use('/api', usersRouter);
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,25 +1,25 @@
 const router = require('express').Router()
 
 
-router.get('/users', (req, res) => {
+router.get('/', (req, res) => {
     // router code here
     res.status(200).json({ message: 'get users' })
 })
 
 
-router.get('/users/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     res.status(200).json({ message: 'get user', id: req.params.id })
 })
 
-router.post('/users', (req, res) => {
+router.post('/', (req, res) => {
     res.status(200).json({ message: 'post users' })
 })
 
-router.put('/users/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     res.status(200).json({ message: 'put users' })
 })
 
-router.delete('/users/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     res.status(200).json({ message: 'delete users' })
 })
 
