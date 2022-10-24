f4c014c9fd87046b5b3cb4712258235a61c59646
eugenio <eugenio@baliotech.com>
2022-10-24 13:28:26 -0500

    isRoleValid validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const { getUsuarios, getUsuario, createUsuario, updateUsuario, deleteUsuario } = require('../../controllers/users')
 const { check } = require('express-validator');
-const Rol = require('../../models/rol')
 const validateUser = require('../../middlewares/userValidator');
+const { isRoleValid } = require('../../helpers/dbValidators');
 const router = require('express').Router()
 
 // Get all users
@@ -17,14 +17,7 @@ router.post('/',
         check('email', 'Email is not valid').isEmail(),
         check('password', 'Password at least 6 characters').notEmpty().isLength({ min: 6 }),
         // check('rol', 'This is not a valid rol').isIn(['ADMIN_ROLE', 'USER_ROLE']),
-        check('rol').custom(async (rol = '') => {
-            console.log({ rol })
-            const rolExist = await Rol.findOne({ rol })
-            console.log(rolExist)
-            if (!rolExist) {
-                throw new Error(`This ${rol} is not register in DB`)
-            }
-        }),
+        check('rol').custom(isRoleValid),
         validateUser
     ],
     createUsuario)
