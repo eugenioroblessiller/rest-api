0534a265d5cd4a5e07d5e72c120abe1b5297210c
eugenio <eugenio@baliotech.com>
2022-10-24 16:12:46 -0500

    validations for update

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,30 +1,33 @@
 const { getUsuarios, getUsuario, createUsuario, updateUsuario, deleteUsuario } = require('../../controllers/users')
 const { check } = require('express-validator');
-const validateUser = require('../../middlewares/userValidator');
-const { isRoleValid, emailExist } = require('../../helpers/dbValidators');
+const { isRoleValid, emailExist, userExistById } = require('../../helpers/dbValidators');
+const validateFields = require('../../middlewares/userValidator');
 const router = require('express').Router()
 
 // Get all users
 router.get('/', getUsuarios)
 
 // Get one user
-router.get('/:id', getUsuario)
+router.get('/:id', [], getUsuario)
 
 // Create a user
-router.post('/',
-    [
-        check('name', 'Name is required').notEmpty(),
-        check('email', 'Email is not valid').isEmail(),
-        check('email').custom(emailExist),
-        check('password', 'Password at least 6 characters').notEmpty().isLength({ min: 6 }),
-        // check('rol', 'This is not a valid rol').isIn(['ADMIN_ROLE', 'USER_ROLE']),
-        check('rol').custom(isRoleValid),
-        validateUser
-    ],
-    createUsuario)
+router.post('/', [
+    check('name', 'Name is required').notEmpty(),
+    check('email', 'Email is not valid').isEmail(),
+    check('email').custom(emailExist),
+    check('password', 'Password at least 6 characters').notEmpty().isLength({ min: 6 }),
+    // check('rol', 'This is not a valid rol').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom(isRoleValid),
+    validateFields
+], createUsuario)
 
 // Update a user
-router.put('/:id', updateUsuario)
+router.put('/:id', [
+    check('id', 'Is not an valid id').isMongoId(),
+    check('id').custom(userExistById),
+    check('rol').custom(isRoleValid),
+    validateFields
+], updateUsuario)
 
 // Delete a user
 router.delete('/:id', deleteUsuario)
