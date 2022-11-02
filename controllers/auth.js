c88c3703d028cbda56bfe9f0eac925aee458f8b4
eugenio <eugenio@baliotech.com>
2022-11-01 23:14:14 -0600

    login user working

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,48 @@
+const bcryptjs = require("bcryptjs");
 const { response } = require("express");
+const jsonwebtoken = require("jsonwebtoken");
+const { generateJsonWebToken } = require("../helpers/generateJsonWebToken");
+const User = require("../models/user");
 
 
-const loginUser = (req, res = response) => {
-    const body = req.body
-    res.status(200).json({ 'message': 'login working', userData: body })
+const loginUser = async (req, res = response) => {
+    const { email, password } = req.body
+
+    try {
+        // check if email exist
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({
+                message: `User with email: ${email} was not found`
+            })
+        }
+        // Check if user is active
+        if (user.status == false) {
+            return res.status(500).json({
+                message: `User with email: ${email} not longer belongs to the system`
+            })
+        }
+        // Check password
+        const validPassword = bcryptjs.compareSync(password, user.password)
+        if (!validPassword) {
+            return res.status(500).json({
+                message: `Password is not correct`
+            })
+        }
+
+        // Generate JWT
+        const token = await generateJsonWebToken(user.id)
+
+        return res.status(200).json({
+            message: "Very very good",
+            user,
+            token
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Something went wrong, call the admin",
+        })
+    }
 }
 
 module.exports = { loginUser }
