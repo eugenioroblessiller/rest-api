1f22e9c7902e093aef932ebe8bfb5f9937e13fb9
eugenio <eugenio@baliotech.com>
2022-11-02 21:01:18 -0600

    returning object change

--- models/category.js
+++ models/category.js
@@ -3,7 +3,27 @@ const { Schema } = mongoose;
 
 
 const categorySchema = new Schema({
-
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        unique: true
+    },
+    state: {
+        type: Boolean,
+        default: true,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
 })
+
+categorySchema.methods.toJSON = function () {
+    const { __v, _id, ...category } = this.toObject()
+    category.id = _id
+    return category
+}
 
 module.exports = mongoose.model('Category', categorySchema)
