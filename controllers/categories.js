a7bc3913a34a8375912b8df54ad3ce6bd7888c3f
eugenio <eugenio@baliotech.com>
2022-11-02 21:01:33 -0600

    create Category works

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,4 +1,5 @@
 const { response } = require("express")
+const Category = require("../models/category")
 
 
 const getCategories = (req, res = response) => {
@@ -11,8 +12,30 @@ const getCategory = (req, res) => {
 
 }
 
-const createCategory = (req, res) => {
+const createCategory = async (req, res) => {
 
+    const name = req.body.name.toUpperCase()
+
+    const categoryDB = await Category.findOne({ name })
+
+    if (categoryDB) {
+        return res.status(400).json({
+            message: `Category ${categoryDB.name} is already created`
+        })
+    }
+
+    const data = {
+        name,
+        user: req.user.id
+    }
+
+    const category = new Category(data)
+    await category.save()
+
+    return res.status(201).json({
+        message: 'Categories is working - POST',
+        category
+    })
 }
 
 const updateCategory = (req, res) => {
