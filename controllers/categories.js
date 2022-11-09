9753617e4159c114b295a0311272f6994bfa6764
eugenio <eugenio@baliotech.com>
2022-11-09 10:57:23 -0600

    get categories and get category

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,23 +1,48 @@
-const { response } = require("express")
+const { response, request } = require("express")
 const Category = require("../models/category")
 
 
-const getCategories = (req, res = response) => {
+const getCategories = async (req, res = response) => {
+    const { limit = 5, from = 0 } = req.query
+    const query = { state: true }
+
+    const [categoryCounts, categories] = await Promise.all([
+        Category.countDocuments(),
+        Category.find(query)
+            .populate('user', 'name')
+            .limit(+limit)
+            .skip(+from)
+            .sort('createdAt')
+    ])
+
     return res.status(200).json({
-        message: 'Categories is working'
+        message: 'Categories is working',
+        categories,
+        categoryCounts
     })
 }
 
-const getCategory = (req, res) => {
+const getCategory = async (req = request, res = response) => {
+    const id = req.params.id
+    const category = await Category.findById(id)
+        .populate('user', 'name')
 
+    if (!category) {
+        return res.status(400).json({
+            message: "Category was not found"
+        })
+    }
+
+    return res.status(200).json({
+        message: "Get Category",
+        category
+    })
 }
 
 const createCategory = async (req, res) => {
-
     const name = req.body.name.toUpperCase()
 
     const categoryDB = await Category.findOne({ name })
-
     if (categoryDB) {
         return res.status(400).json({
             message: `Category ${categoryDB.name} is already created`
